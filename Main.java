ea9842b296b2054ddbe01587edbb039997071c73
hkrishg <109535284+hkrishg@users.noreply.github.com>
2022-10-12 23:28:55 +0000

    o(n) solution

--- Main.java
+++ Main.java
@@ -23,17 +23,59 @@ import java.util.*;
 // Explanation: The whole array needs to be sorted.
 
 class Main {
-    public static int findUnsortedSubarray(int[] nums) {
-        int[] snums = nums.clone();
-        Arrays.sort(snums);
-        int start = snums.length, end = 0;
-        for (int i = 0; i < snums.length; i++) {
-            if (snums[i] != nums[i]) {
-                start = Math.min(start, i);
-                end = Math.max(end, i);
-            }
-        }
-        return (end - start >= 0 ? end - start + 1 : 0);
+    public static int findUnsortedSubarray(int[] arr) {
+      int n = arr.length;
+      int s = 0, e = n-1, i, max, min;   
+        
+      // step 1(a) of above algo
+      for (s = 0; s < n-1; s++)
+      {
+        if (arr[s] > arr[s+1])
+          break;
+      }
+      if (s == n-1)
+      {
+        System.out.println("The complete array is sorted");
+        return 0;
+      }
+        
+      // step 1(b) of above algo
+      for(e = n - 1; e > 0; e--)
+      {
+        if(arr[e] < arr[e-1])
+          break;
+      }
+        
+      // step 2(a) of above algo
+      max = arr[s]; min = arr[s];
+      for(i = s + 1; i <= e; i++)
+      {
+        if(arr[i] > max)
+          max = arr[i];
+        if(arr[i] < min)
+          min = arr[i];
+      }
+        
+      // step 2(b) of above algo
+      for( i = 0; i < s; i++)
+      {
+        if(arr[i] > min)
+        {  
+          s = i;
+          break;
+        }      
+      } 
+        
+      // step 2(c) of above algo
+      for( i = n -1; i >= e+1; i--)
+      {
+        if(arr[i] < max)
+        {
+          e = i;
+          break;
+        } 
+      }  
+        return e - s + 1;
     }
 
   
